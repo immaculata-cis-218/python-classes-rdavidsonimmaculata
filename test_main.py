a61db07d5802627cea5fa4e43b121e79f58dbaca
rdavidsonimmaculata <rdavidson1@mail.immaculata.edu>
2023-03-25 15:58:01 -0400

    Week 8

---
+++ test_main.py
@@ -0,0 +1,26 @@
+"""
+Program Name: Test for Classes
+Author: Rebecca Davidson
+Date: 3/20/2023
+Description: This program will display bank account summary including balances and subshares
+"""
+
+from main import Account
+
+def test_account_number():
+    """
+    Test that the account number is correct
+    """
+    test_member = Account("246812","Test McTest")
+    summary = test_member.account_summary()
+    assert "246812" in summary
+
+
+def test_name():
+    """
+    Test that the account number is correct
+    """
+    test_member2 = Account("3691113","Testianna Testerson")
+    summary2 = test_member2.account_summary()
+    assert "Testianna Testerson" in summary2
+    
