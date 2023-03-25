e21855377d23a48e3c91c98ad470f582d9f5c47d
rdavidsonimmaculata <rdavidson1@mail.immaculata.edu>
2023-03-25 16:01:34 -0400

    Week8

--- main.py
+++ main.py
@@ -2,20 +2,23 @@
 Program Name: Credit Union Accounts
 Author: Rebecca Davidson
 Date: 3/20/2023
-Description: This program will display bank account summary including balances and subshares
+Description: This program will display a member's bank account summary including balances and subshares
 """
+
 
 class Account:
     """
     Credit Union Account
     """
-    def __init__(self, account_number,full_name):
+
+    def __init__(self, account_number, full_name):
         self.__account_number = account_number
         self.full_name = full_name
 
     def account_summary(self):
         return f"{self.full_name}'s account number is {self.__account_number}"
 
+
 if __name__ == "__main__":
-    member = Account("123456","Luis Santiago")
-    print (member.account_summary())
+    member = Account("123456", "Luis Santiago")
+    print(member.account_summary())
